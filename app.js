9cca283b92dec1122a92ba3975506a6840080b85
ProgramacionDH <marcelocanay@hotmail.com>
2023-02-01 10:17:29 -0300

    home

--- app.js
+++ app.js
@@ -2,75 +2,51 @@ const express = require("express");
 const path = require("path");
 const methodOverride = require('method-override');
 const session = require('express-session');
-const cookies = require('cookie-parser');
-const cors = require('cors');
 
-const {sequelize} = require ('./src/database/models') 
-sequelize.sync({alter:false}).then(()=> console.log ('modelossincronizados'));
-
-const userLoggedMiddleware = require('./Middlewares/userLoggedMiddleware');
-//const logger = require('morgan');
-const createError = require('http-errors');
-
-//express//
+const {sequelize} = require ('./database/models') 
+sequelize.sync({alter:false}).then(()=> console.log ('ModeloSincronizados'));
 
 const app = express();
 
-//const methodOverride = require('method-override');
-
-//middleware//
-app.use( express.static(path.join(__dirname, '../public')));  
-
-app.use(cors());  
 app.use(express.json());
-app.use(cookies());
-app.use(methodOverride ("_method")); //Procesamiento PUT y DELETE 
+app.use(methodOverride ("_method"));
 
-//template engine - ejs//
+
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
+app.set('views', path.join(__dirname, '/views')); 
 
 app.use(express.urlencoded({extended:true}));
 
 app.use(session({
-  secret: 'shh, is a secret',
+  secret: 'shhh, es un secreto',
   resave:false ,
   saveUninitialized: false,
 }));
 
 
+const apiRoutes = require('./routes/apiRouter')
+app.use('/', apiRoutes)
 
-//Routes//
-const mainRouter = require('./src/routes/mainRouter');
+const userRouter = require('./src/routes/userRouter');
 
-
-app.use("/", mainRouter); 
-
-
-app.use(userLoggedMiddleware);
+app.use('/user', userRouter); 
 
 const publicPath = path.resolve(__dirname, './public');
-
 app.use( express.static(publicPath));
 
-app.use(methodOverride ("_method")); //Procesamiento PUT y DELETE
+app.use(methodOverride ("_method")); 
 
-//ejs//
+
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
-
-//error//
-
+app.set('views', path.join(__dirname, '/views'));
 app.use((req, res, next) => {
-  res.status(404).render("not-found")
-});
+    res.status(404).render("not-found")
+  });
 
-//listen//
 
 app.listen(3000, () => {
-  console.log("servidor corriendo puerto 3001");
+  console.log("servidor corriendo puerto 3002");
 });
 
-// exports
 
 module.exports = app;

e941b79077e275b48d606924dcc8db3025b7c480
ProgramacionDH <marcelocanay@hotmail.com>
2023-02-20 20:27:21 -0300

    avance

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var productosRouter = require('./routes/productos');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const productosRouter = require('./routes/productos');
 
 var app = express();
 

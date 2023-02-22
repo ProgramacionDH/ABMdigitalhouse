645feabcbb7a905fd45f145088d8f5e519d81b39
ProgramacionDH <marcelocanay@hotmail.com>
2023-02-22 20:09:06 -0300

    midle

--- middleware/validateRegisterMiddleware.js
+++ middleware/validateRegisterMiddleware.js
@@ -4,7 +4,7 @@ const path = require('path');
 let validationsRegister = [
     body('nombre').notEmpty().withMessage('El campo Nombre y Apellido no puede estar vacio'),
     body('email')
-    .notEmpty().withMessage('El campo Email no puede estar vacio')//.bail()
+    .notEmpty().withMessage('El campo Email no puede estar vacio')
     .isEmail().withMessage('Debes escribir un mail valido'),
     body('contrasena1').notEmpty().withMessage('El campo Contrasena no puede estar vacio'),
     body('filename').custom((value, {req})=>{
